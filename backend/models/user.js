1a5a74d833eae92d32f89af0f05ebc60df4ea29e
mrmehdipour <123828759+mrmehdipour@users.noreply.github.com>
2025-01-27 23:32:35 +0330

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  points: { type: Number, default: 0 }, // امتیازات کاربر
+  completedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }], // دوره‌های تکمیل‌شده
 });
 
 module.exports = mongoose.model("User", userSchema);
